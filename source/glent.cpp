9b691521bb2ddb114f32f4bf5919acac5e367c3e
vladeemerr <50291143+vladeemerr@users.noreply.github.com>
2025-06-29 00:18:33 +0300

    Initialize GLFW and create window, load OpenGLES 3.1 via GLAD

---
+++ source/glent.cpp
@@ -0,0 +1,59 @@
+#include <cstdint>
+#include <iostream>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gles2.h>
+
+namespace {
+
+constexpr int32_t window_default_width = 1280;
+constexpr int32_t window_default_height = 720;
+constexpr char window_title[] = "Glent";
+
+GLFWwindow* window;
+
+} // namespace
+
+int main() {
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW\n";
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_RESIZABLE, false);
+	glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_DOUBLEBUFFER, true);
+
+	window = glfwCreateWindow(window_default_width, window_default_height,
+	                          window_title, nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window\n";
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLES2(glfwGetProcAddress)) {
+		std::cerr << "Failed to load GLES 3.1 functions\n";
+		return 1;
+	}
+
+	glfwSwapInterval(1);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
